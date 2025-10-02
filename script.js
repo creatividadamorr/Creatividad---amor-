b7fd8ef23e2bde255a029da4cd3e79def848299c
creatividadamorr <creatividad.amorr@gmail.com>
2025-10-01 21:08:27 -0500

    Crjsjseate script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Simulación simple de carrito
+document.querySelectorAll(".btn").forEach(boton => {
+  boton.addEventListener("click", () => {
+    alert("Producto agregado al carrito 🛒");
+  });
+});
